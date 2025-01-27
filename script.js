0e024efcc64fdfabb1ea397bb3b23ada62d6c07d
Neha00731 <neharkpathak31@gmail.com>
2025-01-27 12:51:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to show or hide the details when a timeline item is clicked
+function showDetails(itemId) {
+    const content = document.getElementById(itemId);
+    content.classList.toggle('visible');
+}
